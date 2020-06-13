72849db7d66a9783e08f720c513d6b19fa95c6c1
root <igomine@126.com>
2020-06-13 14:48:49 +0800

    MOTOR TEST

---
+++ Aliyun/WateringRobot/step_motor_test.py
@@ -0,0 +1,35 @@
+import time
+import RPi.GPIO as GPIO
+
+RightWheelEn = 2
+RightWheelCLK = 3
+RightWheelCW = 15
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(RightWheelEn, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(RightWheelCW, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RightWheelCLK, GPIO.OUT, initial=GPIO.LOW)
+
+
+RightWheelPWM = GPIO.PWM(RightWheelCLK, 50)
+RightWheelPWM.start(50)
+
+GPIO.output(RightWheelEn, GPIO.LOW)
+GPIO.output(RightWheelEn, GPIO.HIGH)
+GPIO.output(RightWheelEn, GPIO.LOW)
+
+# GPIO.setup(RightWheelCLK, GPIO.OUT)
+
+
+
+
+
+GPIO.output(RightWheelEn, GPIO.HIGH)
+GPIO.output(RightWheelCW, GPIO.HIGH)
+# while True:
+
+    # RightWheelPWM.ChangeDutyCycle(dc)
+    # RightWheelPWM.ChangeFrequency(f)
+    # print(GPIO.input(2))
+    # time.sleep(1)
